b6625f45d68594339b2313836faf4f10ccf5bed6
Joe Tustin <j.tustin@gmail.com>
2019-03-19 15:10:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,24 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+#  if name_hash > 1
+    lowest_value = 2000
+    lowest_name = ""
+    name_hash.collect do |name, value|
+        if value < lowest_value
+          lowest_value = value
+          lowest_name = name
+        end
 
+
+    end
+    puts lowest_name
+    puts lowest_value
+    return lowest_name
+#  end
+  return nil
 end
+
+#key_for_min_value({})
+
+# need to figure out the nil return.  the program is working but the issue is the lowest_value variable.  need a way to trigger that 
